74d327a0761cefae4e54cc7d4132342054efc10c
Jaume <jaume.montagut.i.guix@gmail.com>
2018-03-15 22:47:43 +0100

    Added correct distribution file

---
+++ SDL1_handout/SDL1_Handout/Application.h
@@ -0,0 +1,89 @@
+#ifndef __APPLICATION_H__
+#define __APPLICATION_H__
+
+#include "Globals.h"
+#include "Module.h"
+#include "Dummy.h"
+#include "DummyESC.h"
+
+#define NUM_MODULES 2
+
+class Application
+{
+public:
+
+	Module* modules[NUM_MODULES];
+
+public:
+
+	Application()
+	{
+		modules[0] = new ModuleDummy();
+		modules[1] = new ModuleDummyESC();
+		// TODO 7: Create your new module "DummyESC"
+		// it should check if player it ESC key use kbhit()
+		// http://www.cprogramming.com/fod/kbhit.html
+	}
+
+	~Application()
+	{
+		delete[]modules;
+	}
+
+	// INIT all modules
+	bool Init() 
+	{
+		for(int i = 0; i < NUM_MODULES; ++i)
+		{
+			if(modules[i]->Init() == false)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// UPDATE all modules
+	update_status Update()
+	{
+		for (int i = 0; i < NUM_MODULES; i++)
+		{
+			if (modules[i]->PreUpdate() == UPDATE_STOP)
+			{
+				return UPDATE_STOP;
+			}
+		}
+		for(int i = 0; i < NUM_MODULES; i++)
+		{
+			if (modules[i]->Update() == UPDATE_STOP)
+			{
+				return UPDATE_STOP;
+			}
+		}
+		for (int i = 0; i < NUM_MODULES; i++)
+		{
+			if (modules[i]->PostUpdate() == UPDATE_STOP)
+			{
+				return UPDATE_STOP;
+			}
+		}
+		return update_status::UPDATE_CONTINUE;
+	}
+
+	// EXIT Update 
+	bool CleanUp()
+	{
+		for(int i = NUM_MODULES; i > 0; i--)
+		{
+			if (modules[i]->CleanUp() == false)
+			{
+				return false;
+			}
+			modules[i]->CleanUp();
+		}
+		return true;
+	}
+
+};
+
+#endif // __APPLICATION_H__
---
+++ SDL1_handout/SDL1_Handout/Main.cpp
@@ -0,0 +1,81 @@
+#include "Application.h"
+#include "Globals.h"
+#include "MemLeaks.h"
+
+enum main_states
+{
+	MAIN_CREATION,
+	MAIN_START,
+	MAIN_UPDATE,
+	MAIN_FINISH,
+	MAIN_EXIT
+};
+
+int main()
+{
+	ReportMemoryLeaks();
+
+	int main_return = EXIT_FAILURE;
+	main_states state = MAIN_CREATION;
+	Application* App = nullptr;
+
+	while (state != MAIN_EXIT)
+	{
+		switch (state)
+		{
+		case MAIN_CREATION:
+		{
+			LOG("\nApplication Creation --------------");
+			App = new Application();
+			state = MAIN_START;
+		}	break;
+
+		case MAIN_START:
+		{
+			LOG("\nApplication Start --------------");
+			if (App->Init() == false)
+			{
+				LOG("\nApplication Init exits with error -----");
+				state = MAIN_EXIT;
+			}
+			else
+			{
+				state = MAIN_UPDATE;
+			}
+		}	break;
+
+		case MAIN_UPDATE:
+		{
+			update_status status = App->Update();
+
+			if (status == update_status::UPDATE_ERROR)
+			{
+				LOG("\nApplication Update exits with error -----");
+				state = MAIN_EXIT;
+			}
+			else if (status == update_status::UPDATE_STOP)
+			{
+				state = MAIN_FINISH;
+			}
+		} break;
+		case MAIN_FINISH:
+		{
+			LOG("\nApplication Finish --------------");
+			if (App->CleanUp() == false)
+			{
+				LOG("\nApplication Finish exits with error -----");
+				state = MAIN_EXIT;
+			}
+			else
+			{
+				delete(App);
+				state = MAIN_EXIT;
+				main_return = EXIT_SUCCESS;
+			} 
+		}break;
+		}
+	}
+
+	LOG("\nBye :)\n");
+	return main_return;
+}
